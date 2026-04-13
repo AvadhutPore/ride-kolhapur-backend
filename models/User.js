cfcc23446889d3747131f9bfbee44dda3ac70570
AvadhutPore <poreavadhut@gmail.com>
2026-04-13 21:27:30 +0530

    Add Role

--- models/User.js
+++ models/User.js
@@ -3,6 +3,12 @@ const mongoose = require("mongoose");
 const userSchema = new mongoose.Schema({
   name: String,
   mobile: String,
+
+  role: {
+    type: String,
+    enum: ["user", "driver"],
+    default: "user", // 👈 normal users by default
+  },  
 });
 
 module.exports = mongoose.model("User", userSchema);
